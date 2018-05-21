b54e85dc208054a342b07a8aa8dc881f75e3a75c
Serg_kit <sergey-kitaev.vrn@yandex.ru>
2018-05-21 08:25:00 +0300

    Cube update...

--- sound103/Src/main.c
+++ sound103/Src/main.c
@@ -319,6 +319,7 @@ static void MX_TIM2_Init(void)
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 1125;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
